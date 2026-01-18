70e694efd026a982e96c60cc5d3ccd3636705ae8
Jalilur Rahman Murtuza <murtuza.rahman@gmail.com>
2026-01-18 22:27:43 +0600

    #1 - Chore: initialize Spring Boot project with required dependencies from Spring Initializr

---
+++ src/main/java/com/bazario/api/BazarioApplication.java
@@ -0,0 +1,13 @@
+package com.bazario.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bazario/api/BazarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazario.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
