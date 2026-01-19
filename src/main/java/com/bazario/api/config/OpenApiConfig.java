2a94bb3ceebde5ebca9fe39c41f145a6fa195fea
Jalilur Rahman Murtuza <murtuza.rahman@gmail.com>
2026-01-20 00:06:08 +0600

    Chore: externalize API version in OpenAPI config as requested in PR review

--- src/main/java/com/bazario/api/config/OpenApiConfig.java
+++ src/main/java/com/bazario/api/config/OpenApiConfig.java
@@ -2,19 +2,28 @@ package com.bazario.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${api.info.title}")
+    private String title;
+
+    @Value("${api.info.description}")
+    private String description;
+
+    @Value("${api.info.version}")
+    private String version;
 
     @Bean
     public OpenAPI bazarioOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Bazario API")
-                        .description("E-commerce platform API documentation")
-                        .version("v1.0"));
+                        .title(title)
+                        .description(description)
+                        .version(version));
     }
 }
 
