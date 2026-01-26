72251eb1e56050b95d5cf34d6f730bfc69ec6ba7
Jalilur Rahman Murtuza <murtuza.rahman@gmail.com>
2026-01-26 20:47:58 +0600

    #7 - Feat: add UserRepository and RoleRepository with email and name lookup methods

---
+++ src/main/java/com/bazario/api/modules/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bazario.api.modules.auth.repository;
+
+import com.bazario.api.modules.auth.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
