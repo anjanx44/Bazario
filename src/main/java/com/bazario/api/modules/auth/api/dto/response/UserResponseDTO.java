4465cb41b58b8718eb0d573277705663e78d014c
Jalilur Rahman Murtuza <murtuza.rahman@gmail.com>
2026-01-26 21:52:26 +0600

    #7 - Feat: add User and Role request/response DTOs with validation constraints

---
+++ src/main/java/com/bazario/api/modules/auth/api/dto/response/UserResponseDTO.java
@@ -0,0 +1,30 @@
+package com.bazario.api.modules.auth.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private UUID id;
+    private String email;
+    private String FirstName;
+    private String lastName;
+    private String phoneNumber;
+    private Boolean isEnabled;
+    private Boolean isAccountNonLocked;
+    private Boolean isAccountNonExpired;
+    private Boolean isCredentialsNonExpired;
+    private Set<RoleResponseDTO> roles;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+}
